534755c89cd1d85744e0bed29c8caf55476c6115
rathik <rathik@email.chop.edu>
2017-06-01 15:35:47 -0400

    Deleted files manually

--- R/connect_to_server.R
+++
@@ -1,3 +0,0 @@
-library(elastic)
-#connect(es_host = 'ec2-54-161-125-8.compute-1.amazonaws.com',es_path = "",es_port = 9200, es_transport_schema  = "http")
-connect(es_host = 'ec2-54-167-120-156.compute-1.amazonaws.com',es_path = "",es_port = 9200, es_transport_schema  = "http")
