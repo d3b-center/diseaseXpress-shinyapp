e5765b77e5105075dbb590149b42f23c80745147
rathik <rathik@email.chop.edu>
2018-01-04 09:44:34 -0500

    changes how data loads

--- R/loaddata.R
+++ R/loaddata.R
@@ -1,4 +1,1 @@
-# sample.info <- read.delim('data/sample_info_19418.txt', stringsAsFactors = F)
-plot_vars <- read.delim('data/plot_vars.txt', stringsAsFactors = F)
-gene_ann <- read.delim('data/gene_mappings.txt', stringsAsFactors = F)
-# disease <- read.delim('data/disease_list.txt', stringsAsFactors = F)
+load('data/data.RData')
